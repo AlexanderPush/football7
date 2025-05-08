f4d1022e4f0bf0609108b2998f110b5f43a8c648
Flip <31641053+AlexanderPush@users.noreply.github.com>
2025-05-08 19:31:06 +0500

    Update sendWebAppButton.js

--- sendWebAppButton.js
+++ sendWebAppButton.js
@@ -10,7 +10,7 @@ bot.onText(/\/start/, (msg) => {
     reply_markup: {
       keyboard: [[{
         text: 'Открыть матчи',
-        web_app: { url: 'https://football7.onrender.com' } // твой URL Render
+        web_app: { url: 'https://football7.onrender.com' } // Замени на твой актуальный URL на Render
       }]],
       resize_keyboard: true,
       one_time_keyboard: true
